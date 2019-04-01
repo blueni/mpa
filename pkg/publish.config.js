d82ad4b1f40973645da0be35018a5190ff22ae8a
lironghe <lironghe@fujiacaifu.com>
2019-04-01 16:27:50 +0800

    '将与服务器有关的配置放到一个独立的文件里面'

---
+++ pkg/publish.config.js
@@ -0,0 +1,9 @@
+// 这个文件千万不要泄露给其他人知道
+module.exports = {
+    host: '192.168.1.184',
+    port: 22,
+    userName: 'root',
+    password: '123456',
+    localPath: `dist`,
+    remotePath: `/data/test/mpa-test`,
+}
